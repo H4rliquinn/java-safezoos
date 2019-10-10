8d79eed7038a1efba81bb54055365322eb451e4d
Jason Sonnichsen <Harliquinn@gmail.com>
2019-10-10 18:09:45 -0400

    Having a truly awful time with this project for some reason. Finally skipped the first endpoint. Setup FindZooById instead

--- safezoos/src/main/java/com/lambdaschool/zoos/repository/AnimalRepository.java
+++ safezoos/src/main/java/com/lambdaschool/zoos/repository/AnimalRepository.java
@@ -1,12 +1,19 @@
 package com.lambdaschool.zoos.repository;
 
 import com.lambdaschool.zoos.model.Animal;
+import com.lambdaschool.zoos.view.AnimalCount;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 
 public interface AnimalRepository extends CrudRepository<Animal, Long>
 {
     Animal findByAnimaltype(String type);
+
+    @Query(value = "SELECT animaltype||' '||count(a.animalid) as COUNT FROM animal a JOIN zooanimals z ON a.animalID=z.animalid GROUP BY animaltype", nativeQuery = true)
+    AnimalCount countAnimals();
+
 }
---
+++ safezoos/src/main/java/com/lambdaschool/zoos/view/AnimalCount.java
@@ -0,0 +1,9 @@
+package com.lambdaschool.zoos.view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface AnimalCount
+{
+    ArrayList<String> getCount();
+}
